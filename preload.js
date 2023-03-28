594edf6c52d7ab757aeaeb5afead621e972d8d01
Ganeshamoorthy-8681 <127597753+Ganeshamoorthy-8681@users.noreply.github.com>
2023-03-28 13:04:38 +0530

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge , ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('versions', {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
-  updatemessage: (callback)=> ipcRenderer.on("updatemessage",callback)
-  // we can also expose variables, not just functions
-})
